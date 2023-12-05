a323f478ada8caeca005de335e83439ed99b8c77
EYUP <jobmoonday2@gmail.com>
2023-12-05 14:37:21 +0000

    variables have been created

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,21 @@
-import questions;
+let startButtonEl = document.getElementById("start");
+let startScreenDivEl = document.getElementById("start-screen");
+let questionsDivEl = document.getElementById("questions");
+let questionTitleEl = document.getElementById("question-title");
+let choicesDivEl = document.getElementById("choices");
+let endScreenDiv = document.getElementById("end-screen");
+let finalScoreSpanEl = document.getElementById("final-score");
+let submitButtonEl = document.getElementById("submit");
+let initialTextEl = document.getElementById("initials");
+let feedbackDivEl = document.getElementById("feedback");
+let wrapperDiv = document.getElementById("wrapper");
+let timeSpanEl = document.getElementById("time");
+let i=0;
+let score = 0;
+let timer;
+let timerCount = questions.length * 5;
+let isCorrect;
+let scoreArray = [];
 
 
-let timer = document.querySelector("#time");
-let startButton = document.querySelector("#start");
-let question = document.querySelector("#questions");
-let choice = document.querySelector("#choices");
 
-let feedback = document.querySelector("#feedback");
-let initials = document.querySelector("#initials");
-let submit = document.querySelector("#submit");
-
-
-initials
-
-
-submit
