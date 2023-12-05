97fe3bfd29d85a43d6d04894ac3ad0c411e3a4f6
EYUP <jobmoonday2@gmail.com>
2023-12-05 14:24:59 +0000

    folder structure and files have been created

---
+++ assets/js/questions.js
@@ -0,0 +1,15 @@
+ 
+
+ export const questions = [
+    {
+      question: "What is the capital of France?",
+      answers: ["Paris", "Berlin", "Madrid", "Rome"],
+      correctIndex: 0
+    },
+    {
+      question: "Which planet is known as the Red Planet?",
+      answers: ["Venus", "Mars", "Jupiter", "Saturn"],
+      correctIndex: 1
+    },
+    
+  ];
---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+import questions;
+
+
+let timer = document.querySelector("#time");
+let startButton = document.querySelector("#start");
+let question = document.querySelector("#questions");
+let choice = document.querySelector("#choices");
+
+let feedback = document.querySelector("#feedback");
+let initials = document.querySelector("#initials");
+let submit = document.querySelector("#submit");
+
+
+initials
+
+
+submit
